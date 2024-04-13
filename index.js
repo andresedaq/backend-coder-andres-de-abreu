3cdd9a67809e5b0051ab1831ca72b02efbc1d7e1
Andres De Abreu <andresedaqprog@gmail.com>
2024-04-13 09:38:06 -0300

    segunda entrega

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+const fs = require('fs')
+
+
+let pathFile = "./data/products.json"
 let products = [];
 
-const addProducts = (title, description, price, thumbnail, code, stock) => {
+const addProducts = async (title, description, price, thumbnail, code, stock) => {
   const newProduct = {
     id: products.length + 1,
     title,
@@ -23,14 +27,18 @@ const addProducts = (title, description, price, thumbnail, code, stock) => {
   }
 
   products.push(newProduct);
+
+  await fs.promises.writeFile(pathFile,JSON.stringify(products))
 };
 
-const getProducts = () => {
-  console.log(products);
-  return products;
+const getProducts = async () => {
+
+  const productsJson = await fs.promises.readFile(pathFile, "utf8")
+  products = JSON.parse(productsJson) || []
 };
 
-const getProductsById = (id) => {
+const getProductsById = async (id) => {
+    await getProducts()
     const product = products.find(product => product.id === id)
     if(!product) {
       console.log(`Could not find the product with ID ${id}`);
@@ -38,13 +46,35 @@ const getProductsById = (id) => {
     }
 
     console.log(product);
+    return product
 };
 
-addProducts("Samsung Galaxy S21", "A flagship smartphone with top-of-the-line features.", 999, "s21-thumbnail.jpg", "S21", 10);
-addProducts("Samsung Galaxy A52", "A mid-range smartphone with a high-quality camera.", 399, "a52-thumbnail.jpg", "A52", 15);
-addProducts("Samsung Galaxy Note 20", "A premium smartphone with a stylus and powerful performance.", 899, "note20-thumbnail.jpg", "Note20", 8);
-addProducts("Samsung Galaxy Fold", "A foldable smartphone with innovative design and technology.", 1999, "fold-thumbnail.jpg", "Fold", 5);
-addProducts("Samsung Galaxy S20 FE", "A budget-friendly smartphone with flagship-level features.", 599, "s20fe-thumbnail.jpg", "S20FE", 12);
+const updateProduct = async(id, dataProduct) => {
+  await getProducts();
+  const index = products.findIndex(product => product.id === id)
+  products[index] = {
+    ...products[index],
+    ...dataProduct
+  }
+  await fs.promises.writeFile(pathFile, JSON.stringify(products))
+}
 
-getProducts();
-getProductsById(1);
+const deleteProduct = async (id) => {
+  await getProducts();
+  products = products.filter( product => product.id !== id ) 
+  await fs.promises.writeFile(pathFile, JSON.stringify(products))
+}
+
+// addProducts("Samsung Galaxy S21", "A flagship smartphone with top-of-the-line features.", 999, "s21-thumbnail.jpg", "S21", 10);
+// addProducts("Samsung Galaxy A52", "A mid-range smartphone with a high-quality camera.", 399, "a52-thumbnail.jpg", "A52", 15);
+// addProducts("Samsung Galaxy Note 20", "A premium smartphone with a stylus and powerful performance.", 899, "note20-thumbnail.jpg", "Note20", 8);
+// addProducts("Samsung Galaxy Fold", "A foldable smartphone with innovative design and technology.", 1999, "fold-thumbnail.jpg", "Fold", 5);
+// addProducts("Samsung Galaxy S20 FE", "A budget-friendly smartphone with flagship-level features.", 599, "s20fe-thumbnail.jpg", "S20FE", 12);
+
+// // getProducts();
+// getProductsById(2);
+
+// updateProduct(1 , {"id": 1,
+// "title": "Samsung Galaxy S23"})
+
+deleteProduct(1)
